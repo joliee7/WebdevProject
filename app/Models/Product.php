a04bb76ac94c0990e40ec4f13612f8e708e5d8e6
joliee7 <jocelynjoliejap@gmail.com>
2026-04-17 11:04:51 +0700

    week 7 edit and delete product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use App\Models\ProductCategory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    //
+    use SoftDeletes;
     public function product_category():BelongsTo {
         return $this->belongsTo(ProductCategory::class, 'category_id');
     }
--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,13 @@ Route::get('/store', [StoreController::class, 'show'])->name('store');
 Route::get('/about', function() {
     return view('about');
 })->name('about');
+
+Route::get('/product/insert-form', [StoreController::class, 'product_insert_form'])->name('product.insert_form');
+
+Route::post('/product/insert', [StoreController::class, 'insert_product'])->name('insert_product');
+
+Route::get('/product/edit/{product_id}', [StoreController::class, 'product_edit_form'])->name('product_edit_form');
+
+Route::put('/product/update/{product_id}', [StoreController::class, 'update_product'])->name('update_product');
+
+Route::delete('/product/delete/{product_id}', [StoreController::class, 'delete_product'])->name('delete_product');
